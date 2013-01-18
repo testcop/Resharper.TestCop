ee0966f35375e4ba698284cc5d4391a143cae5b1
testcop <testcop@users.noreply.github.com>
2013-01-18 21:48:20 +0000

    added highlight test to ensure we warn when class under test cant be found

---
+++ Project/Src/TestCop.Plugin.Tests/Highlighting/TestFileNameWarningTests.cs.cs
@@ -0,0 +1,35 @@
+using JetBrains.ReSharper.Daemon;
+using JetBrains.Application.Settings;
+using NUnit.Framework;
+using TestCop.Plugin.Highlighting;
+
+namespace TestCop.Plugin.Tests.Highlighting
+{
+    [TestFixture]
+    public class TestFileNameWarningTests : CSharpHighlightingWithinSolutionTestBase
+    {
+        protected override bool HighlightingPredicate(IHighlighting highlighting, IContextBoundSettingsStore settingsstore)
+        {
+            return highlighting is TestFileNameWarning;
+        }
+
+        protected override string RelativeTestDataPath
+        {
+            get { return @"highlighting\sample_sln"; }
+        }
+
+        protected override string SolutionName
+        {
+            get { return @"TestApplication.sln"; }
+        }
+
+        [Test]
+        [TestCase(@"<TestApplication.Tests>\Samples\ClassB.SecurityTests.cs")]
+        [TestCase(@"<TestApplication.Tests>\ClassATests.cs")]       
+        public void Test(string testName)
+        {
+            DoTestFiles(testName);
+        }
+       
+    }
+}
---
+++ Project/Src/test/data/highlighting/sample_sln/TestApplication.Tests/Samples/ClassB.SecurityTests.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCop.TestApplication.Tests.Samples
+{
+    [TestClass]
+    public class ClassBSecurityTests
+    {
+    }
+}
