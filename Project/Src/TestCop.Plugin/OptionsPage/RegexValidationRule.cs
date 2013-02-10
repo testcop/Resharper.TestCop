3a2bb3975a19a6ecf4925f9fe23f0989e2cfc90c
testcop <testcop@users.noreply.github.com>
2013-02-10 21:18:36 +0000

    Added support for different class and namespace suffixes

---
+++ Project/Src/TestCop.Plugin/OptionsPage/RegexValidationRule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace TestCop.Plugin.OptionsPage
+{
+    public class RegexValidationRule : ValidationRule
+    {
+        public string RegexText { get; set; }
+        public string ErrorMessage { get; set; }
+        public RegexOptions RegexOptions { get; set; }
+
+        public override ValidationResult Validate(object value,
+                                                  CultureInfo cultureInfo)
+        {
+            ValidationResult result = ValidationResult.ValidResult;
+           
+            if (!String.IsNullOrEmpty(this.RegexText))
+            {              
+                string text = value as string ?? String.Empty;
+
+               
+                if (!Regex.IsMatch(text, this.RegexText, this.RegexOptions))
+                    result = new ValidationResult(false, this.ErrorMessage);
+            }
+
+            return result;
+        }
+    }
+}
