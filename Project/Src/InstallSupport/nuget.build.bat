f9972f533608c1e5dd981c96e6e75d60a4d55091
testcop <testcop@users.noreply.github.com>
2013-05-29 19:02:22 +0000

    added limited support for for non c# (e.g. vb.net) - still need to work on highlightings

--- Project/Src/InstallSupport/nuget.build.bat
+++ Project/Src/InstallSupport/nuget.build.bat
@@ -2,7 +2,7 @@ REM $(ProjectDir)\..\InstallSupport\nuget.build.bat
 @ECHO === === === === === === === ===
 SET NUGET=..\..\..\..\InstallSupport\NuGet.exe
 SET OUTDIR=D:\temp
-SET VER=0.0.0.5-EAP
+SET VER=0.0.0.6-EAP
 
 @ECHO ===NUGET Publishing to %OUTDIR%
 %NUGET% pack TestCop.nuspec -Symbols
