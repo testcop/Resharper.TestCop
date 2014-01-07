7921dd3039d93b44a1641c431bce4405b380990a
testcop <testcop@users.noreply.github.com>
2014-01-07 21:48:52 +0000

    Bug fix : adding scroll bars to options : Issue:1583

--- Project/Src/TestCop.Plugin/Properties/AssemblyInfo.cs
+++ Project/Src/TestCop.Plugin/Properties/AssemblyInfo.cs
@@ -39,8 +39,8 @@ using TestCop.Plugin.Highlighting;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.8.1.1")]
-[assembly: AssemblyFileVersion("1.8.1.1")]
+[assembly: AssemblyVersion("1.8.1.2")]
+[assembly: AssemblyFileVersion("1.8.1.2")]
 
 // The following information is displayed by ReSharper in the Plugins dialog
 [assembly: PluginTitle("TestCop Resharper Plugin")]
