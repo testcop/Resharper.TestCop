202601707ff5f137c25920b20e1369510f335fab
testcop <testcop@users.noreply.github.com>
2014-01-16 21:33:22 +0000

    adding missing test files

---
+++ Project/Src/test/data/highlighting/sample_integration_sln/TestApplication.IntegrationTests/NG1/ClassWithBothTests.cs
@@ -0,0 +1,16 @@
+﻿// // --
+// // -- TestCop http://testcop.codeplex.com
+// // -- License http://testcop.codeplex.com/license
+// // -- Copyright 2014
+// // --
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestApplication.IntegrationTests.NG1
+{
+    [TestClass]
+    public class ClassWithBothTests
+    {
+         
+    }
+}
