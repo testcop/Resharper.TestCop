181109329556f9e3113e458c5d8755ce9d7d1591
testcop <testcop@users.noreply.github.com>
2014-01-30 20:41:13 +0000

    Memory Leak Fix : replaced the BindToContextLive calls with BindToContextTransient

--- Project/Src/TestCop.Plugin/Properties/AssemblyInfo.cs
+++ Project/Src/TestCop.Plugin/Properties/AssemblyInfo.cs
@@ -40,8 +40,8 @@ using TestCop.Plugin.Highlighting;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.8.1.6")]
-[assembly: AssemblyFileVersion("1.8.1.6")]
+[assembly: AssemblyVersion("1.8.1.7")]
+[assembly: AssemblyFileVersion("1.8.1.7")]
 
 // The following information is displayed by ReSharper in the Plugins dialog
 [assembly: PluginTitle("TestCop Resharper Plugin")]
