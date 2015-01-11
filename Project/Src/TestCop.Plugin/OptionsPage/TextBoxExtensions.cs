13d66211a3d2ac23f404282ac8e732747920bd22
SomethingUseful_cp <486135ed70a0b04b69231364782d6eefdadd51c3y9V6PAYW>
2015-01-11 20:13:12 +0000

    moved a little ui logic out into separate code

---
+++ Project/Src/TestCop.Plugin/OptionsPage/TextBoxExtensions.cs
@@ -0,0 +1,55 @@
+﻿// --
+// -- TestCop http://testcop.codeplex.com
+// -- License http://testcop.codeplex.com/license
+// -- Copyright 2015
+// --
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Controls;
+
+namespace TestCop.Plugin.OptionsPage
+{
+    static class TextBoxExtensions
+    {
+        public static void BindWithRegexMatchesValidation(this TextBox tb, TestFileAnalysisSettings testFileAnalysisSettings, string property, string regexString, string errorMessage = "Invalid suffix.")
+        {
+            var binding = new Binding { Path = new PropertyPath(property) };
+            var namespaceRule = new RegexValidationRule
+            {
+                RegexText = regexString,
+                ErrorMessage = errorMessage,
+                RegexOptions = RegexOptions.IgnoreCase,
+                ValidatesOnTargetUpdated = true
+            };
+
+            binding.ValidationRules.Add(namespaceRule);
+            binding.NotifyOnValidationError = true;
+            binding.ValidatesOnDataErrors = true;
+            binding.ValidatesOnExceptions = true;
+            binding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            tb.DataContext = testFileAnalysisSettings;
+            tb.SetBinding(TextBox.TextProperty, binding);
+        }
+
+        public static void BindWithValidationMustBeARegex(this TextBox tb, TestFileAnalysisSettings testFileAnalysisSettings, string property)
+        {
+            var binding = new Binding { Path = new PropertyPath(property) };
+            var namespaceRule = new IsARegexValidationRule
+            {
+                ErrorMessage = "Invalid Regex",
+                RegexOptions = RegexOptions.IgnoreCase,
+                MinimumGroupsInRegex = 2,
+                ValidatesOnTargetUpdated = true
+            };
+
+            binding.ValidationRules.Add(namespaceRule);
+            binding.NotifyOnValidationError = true;
+            binding.ValidatesOnDataErrors = true;
+            binding.ValidatesOnExceptions = true;
+            binding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            tb.DataContext = testFileAnalysisSettings;
+            tb.SetBinding(TextBox.TextProperty, binding);
+        }
+    }
+}
