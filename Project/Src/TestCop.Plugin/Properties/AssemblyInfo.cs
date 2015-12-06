9232f0dea019f19f74265501ebc73d33105df744
testcop <testcop@users.noreply.github.com>
2015-12-06 21:15:22 +0000

    bug fix: added back in the file templates (now saved to the correct folder )

--- Project/Src/TestCop.Plugin/Properties/AssemblyInfo.cs
+++ Project/Src/TestCop.Plugin/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿// --
 // -- TestCop http://testcop.codeplex.com
 // -- License http://testcop.codeplex.com/license
-// -- Copyright 2013
+// -- Copyright 2015
 // --
 
 using System.Reflection;
@@ -39,8 +39,8 @@ using TestCop.Plugin.Highlighting;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.10.0.2")]
-[assembly: AssemblyFileVersion("1.10.0.2")]
+[assembly: AssemblyVersion("1.10.0.3")]
+[assembly: AssemblyFileVersion("1.10.0.3")]
 
 [assembly: RegisterStaticHighlightingsGroup(Highlighter.HighlightingGroup, "Testing", true)]
 [assembly: RegisterConfigurableHighlightingsGroup("Testing", "Testing")]
