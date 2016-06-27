682c334566190310e1ec4cca5dd358757a932838
testcop <testcop@users.noreply.github.com>
2016-06-27 19:59:34 +0000

    extending test cases

---
+++ Project/Src/test/data/SingleTestProjectForManyCodeProject/ClassToTestNavigation/MyCorp.TestApplication3.Tests/DAL/ClassXXTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyCorp.TestApplication3.Tests.DAL
+{
+    [TestClass]
+    public class ClassXXTests
+    {        
+        [TestMethod]
+        public void MyTest()
+        {
+            Assert.Fail("write me");
+        }
+    }
+}
