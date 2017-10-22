8ba0ad82f1dc925f1caddbfe83d4a9863cf48247
TestCop <testcop@users.noreply.github.com>
2017-10-22 19:38:35 +0100

    update for build file to support missing sourcecode folder

--- Project/Src/InstallSupport/nuget.build.bat
+++ Project/Src/InstallSupport/nuget.build.bat
@@ -5,6 +5,8 @@ SET NUGET=..\..\..\..\InstallSupport\NuGet.exe
 SET OUTDIR=C:\SourceCode\
 SET VER=%1-EAP
 
+IF NOT EXIST %OUTDIR%\NUL OUTDIR=%TEMP%
+
 @ECHO ===NUGET Publishing Version %VER% to %OUTDIR%
 %NUGET% pack -Symbols -Version %VER% TestCop.nuspec
 %NUGET% push Resharper.TestCop.R9.%VER%.symbols.nupkg -ApiKey XXX -Source %OUTDIR%
