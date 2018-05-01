e8855a6e9fb6df6f504bd1942c9952774a0e5b60
Reporting for duty <testcop@users.noreply.github.com>
2018-05-01 21:09:37 +0100

    R#2017.3 Recompile and corrected broken http help links (#16)

--- Project/Src/TestCop.Plugin/Properties/AssemblyInfo.cs
+++ Project/Src/TestCop.Plugin/Properties/AssemblyInfo.cs
@@ -39,8 +39,8 @@ using TestCop.Plugin.Highlighting;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.10.4.8")]
-[assembly: AssemblyFileVersion("1.10.4.8")]
+[assembly: AssemblyVersion("1.10.5.1")]
+[assembly: AssemblyFileVersion("1.10.5.1")]
 [assembly: RegisterStaticHighlightingsGroup(Highlighter.HighlightingGroup, "Testing", true)]
 [assembly: RegisterConfigurableHighlightingsGroup("Testing", "Testing")]
     
