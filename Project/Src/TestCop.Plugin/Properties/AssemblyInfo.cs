4aa049358d11e84ff03b50509362ca0c0965712d
Reporting for duty <testcop@users.noreply.github.com>
2018-05-01 23:45:47 +0100

    Update AssemblyInfo.cs
    increased version number

--- Project/Src/TestCop.Plugin/Properties/AssemblyInfo.cs
+++ Project/Src/TestCop.Plugin/Properties/AssemblyInfo.cs
@@ -18,7 +18,7 @@ using TestCop.Plugin.Highlighting;
 [assembly: AssemblyProduct("TestCop")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyCopyright("Copyright 2017")]
+[assembly: AssemblyCopyright("Copyright 2018")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -39,8 +39,8 @@ using TestCop.Plugin.Highlighting;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.10.5.1")]
-[assembly: AssemblyFileVersion("1.10.5.1")]
+[assembly: AssemblyVersion("1.10.6.1")]
+[assembly: AssemblyFileVersion("1.10.6.1")]
 [assembly: RegisterStaticHighlightingsGroup(Highlighter.HighlightingGroup, "Testing", true)]
 [assembly: RegisterConfigurableHighlightingsGroup("Testing", "Testing")]
     
