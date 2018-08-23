010e17d3b400c9701934158dfec40487c376276f
Bruno Lorenz Juchli <bruno.lorenz.juchli@erowa.com>
2018-08-23 09:56:43 +0200

    fixing compilation issues

--- Project/Src/TestCop.Plugin.Tests/TestEnvironmentAssembly.cs
+++ Project/Src/TestCop.Plugin.Tests/TestEnvironmentAssembly.cs
@@ -1,34 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using JetBrains.Application;
-using JetBrains.Application.BuildScript.Application;
-using JetBrains.Application.BuildScript.Application.Zones;
-using JetBrains.Application.BuildScript.PackageSpecification;
-using JetBrains.Application.BuildScript.Solution;
-using JetBrains.Application.Environment;
-using JetBrains.Application.Environment.HostParameters;
-using JetBrains.Metadata.Reader.API;
-using JetBrains.Metadata.Utils;
+﻿using JetBrains.Application.BuildScript.Application.Zones;
 using JetBrains.ReSharper.TestFramework;
 using JetBrains.TestFramework;
 using JetBrains.TestFramework.Application.Zones;
-using JetBrains.TestFramework.Utils;
-using JetBrains.Util;
 using NUnit.Framework;
 
 [assembly: RequiresSTA]
 
 namespace TestCop.Plugin.Tests
-{    
+{
     [ZoneDefinition]
-    public interface ITestCopTestZone : ITestsZone, IRequire<PsiFeatureTestZone>
-    {}
+    public interface ITestCopTestZone : ITestsEnvZone, IRequire<PsiFeatureTestZone>
+    { }
 
     [SetUpFixture]
     public class TestEnvironmentAssembly : ExtensionTestEnvironmentAssembly<ITestCopTestZone>
     {
     }
-
 }
