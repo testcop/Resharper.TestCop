e0b58a7e742702754ed6468135e9d49d47481825
TestCop <testcop@users.noreply.github.com>
2018-09-05 22:12:44 +0100

    Compile process now works out Resharper Wave Id for nuspec file

--- Project/Src/InstallSupport/nuget.build.bat
+++ Project/Src/InstallSupport/nuget.build.bat
@@ -9,7 +9,12 @@ IF NOT EXIST %OUTDIR%\NUL SET OUTDIR=%TEMP%
 
 IF NOT EXIST %OUTDIR%\NUL OUTDIR=%TEMP%
 
+REM Extract Wave Version from Package. JetBrains.Platform.Sdk.*.*.*.*
+SET VV=DIR /B ..\..\..\..\..\Packages\JetBrains.Platform.Sdk.*
+for /f "usebackq tokens=4 delims=." %%a in (`%VV%`) do SET WAVEID=%%a
+@ECHO Resharper Platform WAVE Id=%WAVEID%
+
 @ECHO ===NUGET Publishing Version %VER% to %OUTDIR%
-%NUGET% pack -Symbols -Version %VER% TestCop.nuspec
+%NUGET% pack -Symbols -Version %VER% TestCop.nuspec -properties WAVEID=%WAVEID%
 %NUGET% push Resharper.TestCop.R9.%VER%.symbols.nupkg -ApiKey XXX -Source %OUTDIR%
 @ECHO === === === === === === === ===
