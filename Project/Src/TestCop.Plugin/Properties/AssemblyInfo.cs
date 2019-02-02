0912739e8360ae3f642eca5cbec996542ec1397a
TestCop <testcop@users.noreply.github.com>
2019-02-02 16:44:58 +0000

    recompile for R#2018.3.2

--- Project/Src/TestCop.Plugin/Properties/AssemblyInfo.cs
+++ Project/Src/TestCop.Plugin/Properties/AssemblyInfo.cs
@@ -39,8 +39,8 @@ using TestCop.Plugin.Highlighting;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.10.6.3")]
-[assembly: AssemblyFileVersion("1.10.6.3")]
+[assembly: AssemblyVersion("1.10.6.4")]
+[assembly: AssemblyFileVersion("1.10.6.4")]
 [assembly: RegisterStaticHighlightingsGroup(Highlighter.HighlightingGroup, "Testing", true)]
 [assembly: RegisterConfigurableHighlightingsGroup("Testing", "Testing")]
     
