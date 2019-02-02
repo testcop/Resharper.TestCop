8366089427605cbd3d4db78b9e908fd46f8751d3
Reporting for duty <testcop@users.noreply.github.com>
2019-02-02 16:58:58 +0000

    Development (#31)
    * recompile and minor changes to support R#2018.2
    
    * Corrected all codeplex links to point to github
    
    * Compile process now works out Resharper Wave Id for nuspec file
    
    * recompile for R#2018.3
    
    * recompile for R#2018.3.2

--- Project/Src/TestCop.Plugin/Properties/AssemblyInfo.cs
+++ Project/Src/TestCop.Plugin/Properties/AssemblyInfo.cs
@@ -39,8 +39,10 @@ using TestCop.Plugin.Highlighting;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.10.6.3")]
-[assembly: AssemblyFileVersion("1.10.6.3")]
+
+[assembly: AssemblyVersion("1.10.6.4")]
+[assembly: AssemblyFileVersion("1.10.6.4")]
+
 [assembly: RegisterStaticHighlightingsGroup(Highlighter.HighlightingGroup, "Testing", true)]
 [assembly: RegisterConfigurableHighlightingsGroup("Testing", "Testing")]
     
