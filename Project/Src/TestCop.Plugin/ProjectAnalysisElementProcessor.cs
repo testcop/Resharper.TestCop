b288349fca0882b8d07da1e04fda19db7d7bb64f
Reporting for duty <testcop@users.noreply.github.com>
2020-06-14 08:29:43 +0100

    Development (#52)
    * bug fix for orpaned files #51

--- Project/Src/TestCop.Plugin/ProjectAnalysisElementProcessor.cs
+++ Project/Src/TestCop.Plugin/ProjectAnalysisElementProcessor.cs
@@ -16,7 +16,9 @@ using JetBrains.ReSharper.Feature.Services.Daemon;
 using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.CSharp.Tree;
 using JetBrains.ReSharper.Psi.Tree;
+using JetBrains.ReSharper.PsiGen.Util;
 using JetBrains.Util;
+using TestCop.Plugin.Helper;
 using TestCop.Plugin.Highlighting;
 
 namespace TestCop.Plugin
@@ -27,7 +29,7 @@ namespace TestCop.Plugin
         private readonly IContextBoundSettingsStore _settings;
 
         private readonly FilteringHighlightingConsumer _highlightingConsumer;
-        
+
         protected void AddHighlighting(DocumentRange range, IHighlighting highlighting)
         {
             _highlightingConsumer.AddHighlighting(highlighting, range);
@@ -38,13 +40,15 @@ namespace TestCop.Plugin
             get { return _highlightingConsumer.Highlightings.AsIReadOnlyList(); }
         }
 
-        public ProjectAnalysisElementProcessor(ProjectAnalysisDaemonStageProcess stageProcess, IDaemonProcess process, IContextBoundSettingsStore settings )
+        public ProjectAnalysisElementProcessor(ProjectAnalysisDaemonStageProcess stageProcess, IDaemonProcess process,
+            IContextBoundSettingsStore settings)
         {
-            _highlightingConsumer = new FilteringHighlightingConsumer(stageProcess.File.GetSourceFile(), stageProcess.File , settings);
+            _highlightingConsumer = new FilteringHighlightingConsumer(stageProcess.File.GetSourceFile(), stageProcess.File, settings);
+            
             _process = process;
-            _settings = settings;            
+            _settings = settings;
         }
-        
+
         public bool InteriorShouldBeProcessed(ITreeNode element)
         {
             return false;
@@ -55,53 +59,68 @@ namespace TestCop.Plugin
         }
 
         public void ProcessAfterInterior(ITreeNode element)
-        {            
+        {
             var usingBlock = element as IUsingList;
             if (usingBlock != null)
             {
-                CheckForProjectFilesNotInProjectAndWarn(element);                
-            }                    
+                CheckForProjectFilesNotInProjectAndWarn(element);
+            }
         }
-   
+
         public bool ProcessingIsFinished
         {
-            get {  return _process.InterruptFlag; }
+            get { return _process.InterruptFlag; }
         }
 
         private void CheckForProjectFilesNotInProjectAndWarn(ITreeNode element)
         {
             string[] filesToFind = Settings.OrphanedFilesPatterns.Split('|');
-            if (filesToFind.Length == 0) filesToFind = new []{"*.cs"};
-                        
+            if (filesToFind.Length == 0) filesToFind = new[] {"*.cs"};
+
             var currentProject = element.GetProject();
-            
+            var directoriesToSkip = currentProject.GetOutputDirectories();
+            directoriesToSkip.AddAll(currentProject.GetIntermidiateDirectories());
+
             var allProjectFileLocations = currentProject.GetAllProjectFiles().Select(p => p.Location).ToList();
             var allProjectFiles = allProjectFileLocations.Select(loc => loc.FullPath).ToList();
-            var allProjectFolders = allProjectFileLocations.Select(loc => loc.Directory.FullPath).Distinct();
+            var allProjectFolders = allProjectFileLocations.Select(loc => loc.Directory).Distinct();
 
-            var filesOnDisk = new List<FileInfo>();            
-            filesToFind.ForEach(regex=>filesOnDisk.AddRange(
-                allProjectFolders.SelectMany(
-                    directory => new System.IO.DirectoryInfo(directory).EnumerateFiles(regex, System.IO.SearchOption.TopDirectoryOnly).Select(f => f))
-                        ));
+            allProjectFolders = allProjectFolders.Where(x => !InDirectory(directoriesToSkip, x));
+
+            var filesOnDisk = new List<FileInfo>();
+            filesToFind.ForEach(regex => filesOnDisk.AddRange(
+                                    allProjectFolders.SelectMany(
+                                        directory =>
+                                            new System.IO.DirectoryInfo(directory.FullPath)
+                                                .EnumerateFiles(regex, System.IO.SearchOption.TopDirectoryOnly)
+                                                .Select(f => f))
+                                ));
 
 
             var orphanedFiles = new List<FileInfo>();
 
             foreach (var fileOnDisk in filesOnDisk)
             {
-                if (allProjectFiles.Any(x => String.Compare(x, fileOnDisk.FullName,  StringComparison.OrdinalIgnoreCase) == 0)) continue;
-                orphanedFiles.Add(fileOnDisk);               
+                if (allProjectFiles.Any(
+                    x => String.Compare(x, fileOnDisk.FullName, StringComparison.OrdinalIgnoreCase) == 0)) continue;
+                
+                orphanedFiles.Add(fileOnDisk);
             }
 
             if (orphanedFiles.Count > 0)
             {
                 IHighlighting highlighting = new FilesNotPartOfProjectWarning(currentProject, orphanedFiles);
-                AddHighlighting(element.GetDocumentRange(), highlighting);                                 
+                AddHighlighting(element.GetDocumentRange(), highlighting);
             }
         }
 
-        private TestFileAnalysisSettings Settings
+        public static bool InDirectory(ICollection<FileSystemPath> directoriesToSkip, FileSystemPath fileOnDisk)
+        {
+            if (fileOnDisk == null || directoriesToSkip == null) return false;
+            return directoriesToSkip.Any(x => x.IsPrefixOf(fileOnDisk));
+        }
+
+    private TestFileAnalysisSettings Settings
         {
             get
             {
