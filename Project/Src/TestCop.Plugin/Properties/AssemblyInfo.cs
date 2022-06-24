39c08710ed32c244adcd3a4743f3bdb53027dd30
Raphael Tweitmann <r.tweitmann@neusta.de>
2022-06-24 14:48:28 +0200

    Recompiled with R# SDK 2022.2.0-eap06

--- Project/Src/TestCop.Plugin/Properties/AssemblyInfo.cs
+++ Project/Src/TestCop.Plugin/Properties/AssemblyInfo.cs
@@ -38,6 +38,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.10.16.0")]
-[assembly: AssemblyFileVersion("1.10.16.0")]
+[assembly: AssemblyVersion("1.10.17.0")]
+[assembly: AssemblyFileVersion("1.10.17.0")]
 
