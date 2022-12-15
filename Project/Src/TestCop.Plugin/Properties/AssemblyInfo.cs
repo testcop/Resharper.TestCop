df1040a7e555066bf69a77a37618fa4bcd0f5a5b
Alcaeus <Alcaeus>
2022-12-15 22:09:17 +0000

    recompile

--- Project/Src/TestCop.Plugin/Properties/AssemblyInfo.cs
+++ Project/Src/TestCop.Plugin/Properties/AssemblyInfo.cs
@@ -38,6 +38,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.10.17.1")]
-[assembly: AssemblyFileVersion("1.10.17.1")]
+[assembly: AssemblyVersion("1.10.18.0")]
+[assembly: AssemblyFileVersion("1.10.18.0")]
 
