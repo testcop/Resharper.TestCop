ef058a6ae50b53d46c9fdefdaf568193b07900b3
Bastien Chetelat <bastien.chetelat@liebherr.com>
2023-04-12 11:10:38 +0200

    Update to R# SDK 2023.1.0

--- Project/Src/TestCop.Plugin/Properties/AssemblyInfo.cs
+++ Project/Src/TestCop.Plugin/Properties/AssemblyInfo.cs
@@ -38,6 +38,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.10.18.0")]
-[assembly: AssemblyFileVersion("1.10.18.0")]
+[assembly: AssemblyVersion("1.10.19.0")]
+[assembly: AssemblyFileVersion("1.10.19.0")]
 
