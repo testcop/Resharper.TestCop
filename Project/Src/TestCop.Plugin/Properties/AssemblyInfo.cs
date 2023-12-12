768bec6b1934314806edee8f83ffe3498e89f0ee
Reporting for duty <testcop@users.noreply.github.com>
2023-12-12 21:25:40 +0000

    2023.3.1 (#111)
    Co-authored-by: Alcaeus <Alcaeus>

--- Project/Src/TestCop.Plugin/Properties/AssemblyInfo.cs
+++ Project/Src/TestCop.Plugin/Properties/AssemblyInfo.cs
@@ -38,6 +38,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.10.20.0")]
-[assembly: AssemblyFileVersion("1.10.20.0")]
+[assembly: AssemblyVersion("1.10.21.0")]
+[assembly: AssemblyFileVersion("1.10.21.0")]
 
